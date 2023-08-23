707646ca4c1cb484db002d247d580ba7bd714c93
Mykola_Muntian <mykola.muntian@gmail.com>
2023-08-23 15:58:51 +0300

    Added logic to the service

--- src/main/java/com/example/jokeapp/service/OfficialJokeServiceProviderImpl.java
+++ src/main/java/com/example/jokeapp/service/OfficialJokeServiceProviderImpl.java
@@ -9,6 +9,9 @@ import java.util.List;
 public class OfficialJokeServiceProviderImpl implements JokeServiceProvider{
     @Override
     public List<String> getJoke(int jokesNumber) {
+        String url = "https://official-joke-api.appspot.com/random_joke";
+        String json = "[]";
+
         List<String> jokeList = new ArrayList<>();
         jokeList.add("my first joke");
         return jokeList;
